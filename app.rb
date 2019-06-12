54e738a8e977e26f320dcae58086f3090578fb2e
Savannah C <sgcorrell@gmail.com>
2019-06-12 20:02:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   
   get "/goodbye/:name" do
     @user_name = params[:name]
-    "Goodbye #{@user_name}."
+    "Goodbye, #{@user_name}."
   end
   
   get "/multiply/:number1/:number2" do
